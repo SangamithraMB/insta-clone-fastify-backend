719723e52b2ffb814016a86c634cdee2d098ed19
SangamithraMB <159931656+SangamithraMB@users.noreply.github.com>
2025-08-08 12:25:16 +0200

    port fix

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { postsRoutes } from "./modules/posts/posts.routes";
 import { reelsRoutes } from "./modules/reels/reels.routes";
 import { taggedRoutes } from "./modules/tagged/tagged.routes";
 import { highlightsRoutes } from "./modules/highlights/highlights.routes";
+// import { usersRoutes } from "./modules/users/users.routes";
 
 const fastify = Fastify({
   logger: true,
@@ -33,6 +34,7 @@ fastify.register(postsRoutes);
 fastify.register(reelsRoutes);
 fastify.register(taggedRoutes);
 fastify.register(highlightsRoutes);
+// fastify.register(usersRoutes)
 
 // Declare a default route
 fastify.get("/", function (request, reply) {
@@ -41,7 +43,7 @@ fastify.get("/", function (request, reply) {
 
 const port = Number(process.env.PORT) || 3000;
 
-fastify.listen({ port }, function (err, address) {
+fastify.listen({ port, host: "0.0.0.0" }, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
